0a06a0e4687c365ce08cb9d8e146d7f59d0c5ef0
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2023-08-22 21:34:34 -0300

    ...

--- src/app/components/DoeItem.jsx
+++ src/app/components/DoeItem.jsx
@@ -4,19 +4,19 @@ import Socials from './Socials'
 
 export default function DoeItem({ title, pix, bank }) {
   return (
-    <div className=" flex h-[250px] w-[200px] flex-col items-center justify-between  rounded-xl bg-white p-1 shadow-xl dark:bg-black dark:shadow-dark">
+    <div className=" flex min-h-[350px] w-[250px] flex-col items-center justify-between  rounded-xl bg-white p-1 shadow-xl dark:bg-black dark:shadow-dark">
       <h1 className=" text-xl font-bold text-primary">{title}</h1>
 
       <di className="flex flex-col gap-3">
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-2">
           <BsBank className="text-xl text-primary" /> <p>{bank}</p>
         </div>
 
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-2">
           <MdOutlinePix className="text-xl text-primary" /> <p>{pix}</p>
         </div>
 
-        <BsQrCode className=" flex self-center text-7xl text-primary" />
+        <BsQrCode className=" flex self-center text-9xl text-primary" />
       </di>
       <Socials
         whatsapp="https://api.whatsapp.com/send?phone="
--- src/app/components/Locais.jsx
+++ src/app/components/Locais.jsx
@@ -8,9 +8,11 @@ export default function Locais() {
       <h1 className="m-0 mt-1 text-lg font-bold text-primary">Endereços</h1>
       <p className="mb-5 text-xl">Todos os nossos endereços</p>
       <div className="mb-5 shadow-xl dark:shadow-dark">
-        <div className="flex  w-[230px] flex-col items-center rounded-lg bg-white shadow-lg dark:bg-black dark:shadow-dark md:w-[350px]">
-          <div className="flex w-[80%] flex-1  flex-col justify-center gap-2 p-3">
-            <h1 className="text-2xl font-bold text-primary">Vila da Penha</h1>
+        <div className="flex min-h-[350px]  w-[250px] flex-col items-center rounded-lg bg-white shadow-lg dark:bg-black dark:shadow-dark">
+          <div className="flex  w-[80%]  flex-1  flex-col gap-2 p-2">
+            <h1 className="flex w-full justify-center text-2xl font-bold text-primary">
+              Vila da Penha
+            </h1>
             <p>Rua Tomas Lopes 84, Vila da Penha, Rio de Janeiro</p>
             <p>CEP: 21221210</p>
           </div>
