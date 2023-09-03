732fce85aefcb81b011afba326238e30fce28a49
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2023-09-03 17:41:06 -0300

    consertando bug

--- src/app/components/TimeLine.jsx
+++ src/app/components/TimeLine.jsx
@@ -48,6 +48,7 @@ export default function TimeLine() {
                     hora={item.hora}
                     titletwo={item.titletwo}
                     horatwo={item.horatwo}
+                    trueitem={item.trueitem}
                   />
                 )
               }
--- src/app/components/TimeLineItem.jsx
+++ src/app/components/TimeLineItem.jsx
@@ -1,4 +1,11 @@
-export default function TimeLineItem({ title, titletwo, hora, horatwo, day }) {
+export default function TimeLineItem({
+  title,
+  titletwo,
+  hora,
+  horatwo,
+  day,
+  trueitem,
+}) {
   return (
     <div className="mb-10 ml-3 mr-3 mt-5  flex  min-h-[350px] w-[250px] flex-col items-center rounded-xl bg-white/60 p-3 shadow-xl hover:shadow-lg hover:shadow-primary dark:bg-black/60 dark:shadow-dark dark:hover:shadow-primary md:hidden">
       <h1 className="mb-2 text-2xl text-primary">{day}</h1>
@@ -13,16 +20,18 @@ export default function TimeLineItem({ title, titletwo, hora, horatwo, day }) {
             {hora}
           </p>
         </li>
-        <li className="mb-10 ml-6">
-          <span className="absolute -left-1 flex h-2 w-2 items-center justify-center rounded-full bg-primary ring-8 ring-primary/20"></span>
-          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-            {titletwo}
-          </h3>
+        {trueitem === true && (
+          <li className="mb-10 ml-6">
+            <span className="absolute -left-1 flex h-2 w-2 items-center justify-center rounded-full bg-primary ring-8 ring-primary/20"></span>
+            <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+              {titletwo}
+            </h3>
 
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            {horatwo}
-          </p>
-        </li>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              {horatwo}
+            </p>
+          </li>
+        )}
       </ul>
     </div>
   )
