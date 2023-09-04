234098642321f35919b1c2dd7e72d0636400fe38
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2023-09-04 00:01:50 -0300

    refatorando

--- src/app/service/DataNews.js
+++ src/app/service/DataNews.js
@@ -11,41 +11,41 @@ export const DataNews = [
     id: '1',
     url: domingo,
     title: 'Domingo - Culto de adoração',
-    description: 'Todo domingo, às 10:00/12:00, culto para adorar a Deus',
+    description: 'Todo domingo, de 10:00 até 12:00, culto para adorar a Deus',
   },
   {
     id: '2',
     url: segunda,
     title: 'Segunda - Culto de consagração',
-    description: 'Toda segunda, às 19:30/21:30, culto para adorar Deus',
+    description: 'Toda segunda, de 19:30 até 21:30, culto para adorar Deus',
   },
   {
     id: '3',
     url: quarta,
     title: 'Quarta - Culto em busca do  Espírito Santo',
     description:
-      'Toda quarta, às 18:30/20:30, culto em busca do Espírito Santo. ',
+      'Toda quarta, de 18:30 até 20:30, culto em busca do Espírito Santo. ',
   },
   {
     id: '4',
     url: terça,
     title: 'Terça - projeto jiu-jítsu',
     description:
-      'Toda terça e sexta, de 19:00h até 21:00h, nosso projeto social de Jiujtsu. Venha fazer parte!',
+      'Toda terça e sexta, de 19:00 até 21:00, nosso projeto social de Jiujtsu. Venha fazer parte!',
   },
   {
     id: '5',
     url: jiujtsu,
     title: 'Sexta - projeto jiu-jítsu',
     description:
-      'Toda sexta, de 19:00h até 21:00h, nosso projeto social de Jiujtsu. Venha fazer parte!',
+      'Toda sexta, de 19:00 até 21:00, nosso projeto social de Jiujtsu. Venha fazer parte!',
   },
   {
     id: '6',
     url: comidas,
     title: 'Sábado - dia das delícias',
     description:
-      'Sábado, às 18:30h, dia das delícias. Uma vez por mês, temos nosso evento com diversas comidas para você e sua família',
+      'Sábado, às 18:30, dia das delícias. Uma vez por mês, temos nosso evento com diversas comidas para você e sua família',
   },
   {
     id: '7',
