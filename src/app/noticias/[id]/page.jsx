a53df5d2ea28736e4d10efa6d1065900fb5f7075
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2023-09-11 01:29:39 -0300

    adicionando componente de leia mais notícias em página da notícia

--- src/app/noticias/[id]/page.jsx
+++ src/app/noticias/[id]/page.jsx
@@ -1,29 +1,49 @@
+'use client'
 import Image from 'next/image'
 import { DataNews } from '@/app/service/DataNews'
+import New from '@/app/components/New'
+import { useSearch } from '@/app/store/searchStore'
 
 export default function Noticia({ params }) {
   const id = params.id
+  const { setSearch } = useSearch()
 
   const selectedItem = DataNews.find((item) => item.id === id)
 
   return (
     <main className="flex min-h-screen flex-col  items-center gap-5 pt-24 md:pt-[165px]">
-      <article className="mb-5  flex flex-col items-center">
-        <h1 className="w-[90vw] max-w-[500px] border-b-[1px] border-solid border-primary text-center text-2xl font-bold">
+      <article className="mb-5  flex w-full flex-col items-center border-b-[1px] border-solid border-primary">
+        <h1 className="w-[90vw] max-w-[500px]  text-center text-2xl font-bold">
           {selectedItem.title}
         </h1>
-        <p className=" w-[90vw] max-w-[500px] py-5 text-justify text-lg">
-          {selectedItem.description}
-        </p>
 
         <Image
           src={selectedItem.url}
           alt={selectedItem.title}
           width={500}
           height={500}
-          className="w-[90vw] max-w-[500px]"
+          className="w-[100vw] max-w-[500px] pt-2"
         />
+
+        <p className=" w-[90vw] max-w-[500px] py-5 text-justify text-lg">
+          {selectedItem.description}
+        </p>
       </article>
+      <h1 className="text-2xl font-bold">Leia mais notícias</h1>
+      <div className="flex w-full  flex-wrap justify-center gap-x-5 p-1 md:gap-x-5">
+        {DataNews.reverse()
+          .slice(0, 6)
+          .map((item) => (
+            <New
+              key={item.id}
+              url={item.url}
+              title={item.title}
+              id={item.id}
+              setSearch={setSearch}
+              description={item.description.slice(0, 30)}
+            />
+          ))}
+      </div>
     </main>
   )
 }
