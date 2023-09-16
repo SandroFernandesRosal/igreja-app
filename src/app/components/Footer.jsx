eb24fb30ed5fe5d14ad1712d70a7b96e37dc73e5
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2023-09-16 19:05:05 -0300

    refatorando

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className="flex h-[100px] flex-col items-center justify-evenly border-t-2 border-solid border-y-primary  bg-white/50    dark:bg-bgdarksecundary ">
+    <footer className="flex h-[100px] flex-col items-center justify-evenly border-t-2 border-solid border-y-primary  bg-bglightsecundary    dark:bg-bgdarksecundary ">
       <Link href="/">
         <Image src={logo} alt="logo do site" width={200} height={50} />
       </Link>
