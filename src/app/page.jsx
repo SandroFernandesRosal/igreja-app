428df6d374397f198a6e9d7848de6021c7473cb5
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2023-09-18 01:01:33 -0300

    refatorando

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 import Locais from './components/Locais'
 import NewsPenha from './components/NewsPenha'
-import Link from 'next/link'
 import TimeLine from './components/TimeLine'
 import Doe from './components/Doe'
 import Lideres from './components/Ministerio'
@@ -10,16 +9,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col   items-center gap-5 pt-24 md:pt-[165px]">
       <NewsPenha />
       <Locais />
-      <TimeLine>
-        <Link
-          href={`/agenda`}
-          className="mb-5   h-[30px] w-[150px] rounded-xl shadow-light dark:shadow-dark"
-        >
-          <button className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-primary   font-bold text-black  hover:bg-primary/50    ">
-            Saiba mais
-          </button>
-        </Link>
-      </TimeLine>
+      <TimeLine />
       <Doe />
       <Lideres />
     </main>
