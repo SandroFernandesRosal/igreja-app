b82b8ee913d69cc8ba3ddab8f0570540e4bb8b45
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2023-09-22 20:02:22 -0300

    adicionando ao local storage

--- src/app/store/searchStore.js
+++ src/app/store/searchStore.js
@@ -1,14 +1,28 @@
 import { create } from 'zustand'
 import { DataNews } from '../service/DataNews'
 
+function isLocalStorageAvailable() {
+  try {
+    const testKey = '__zustand_test__'
+    localStorage.setItem(testKey, testKey)
+    localStorage.removeItem(testKey)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
 export const useSearch = create((set) => ({
   search: '',
   setSearch: (query) => set({ search: query }),
-
-  DataNews,
 }))
 
 export const useHook = create((set) => ({
-  data: DataNews,
-  setData: (state) => set({ data: state }),
+  data: JSON.parse(localStorage.getItem(DataNews)) || [], // Inicializa com o valor armazenado no localStorage, se existir
+  setData: (state) => {
+    set({ data: state })
+    if (isLocalStorageAvailable()) {
+      localStorage.setItem(DataNews, JSON.stringify(state)) // Salva o valor no localStorage
+    }
+  },
 }))
