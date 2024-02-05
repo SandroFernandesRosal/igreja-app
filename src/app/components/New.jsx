1a37897cd63a4d98cd051657b0016ad01df1c0ae
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2024-02-04 22:31:29 -0300

    correção de bug

--- src/app/components/New.jsx
+++ src/app/components/New.jsx
@@ -2,12 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { FaLongArrowAltRight } from 'react-icons/fa'
 
-export default function New({ url, title, id, setSearch, description, page }) {
+export default function New({ url, title, id, description, page }) {
   return (
     <article className=" mb-5  flex h-[300px] w-[45%] max-w-[150px] flex-col items-center rounded-lg bg-bglight shadow-light   transition  delay-150 duration-300  ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-hover dark:bg-bgdark dark:shadow-dark dark:hover:shadow-hover  md:h-[300px] md:w-[200px]  md:max-w-[200px]">
       <Link
         href={`/noticias/${page}/${id}`}
-        onClick={() => setSearch('')}
         className="flex-1 border-b-[1px] border-gray-200 dark:border-gray-600"
       >
         <Image
@@ -26,7 +25,6 @@ export default function New({ url, title, id, setSearch, description, page }) {
 
         <Link
           href={`/noticias/${page}/${id}`}
-          onClick={() => setSearch('')}
           className="flex items-center  justify-center gap-2 font-bold text-primary hover:text-primary/40"
         >
           <p>Leia mais</p> <FaLongArrowAltRight className="text-xl" />
