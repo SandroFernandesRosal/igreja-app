8e75cd0674e8b9bb1b2aa2a2d6036a14e91ae299
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2024-02-15 01:02:07 -0300

    ...

--- src/app/noticias/viladapenha/[id]/page.jsx
+++ src/app/noticias/viladapenha/[id]/page.jsx
@@ -113,7 +113,7 @@ export default function NoticiaVilaDaPenha({ params }) {
                   title={item.title}
                   id={item.id}
                   setSearch={setSearch}
-                  description={item.content ? item.content.slice(0, 30) : ''}
+                  description={item.content.slice(0, 30)}
                   page={item.page}
                   data={data}
                   setLocal={setLocal}
