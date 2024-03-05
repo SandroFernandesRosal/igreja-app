c743cb56562a8db5d25e7b24bf58371395737da5
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2024-03-04 23:36:39 -0300

    salvando local no cookie

--- src/components/SelectLocal.jsx
+++ src/components/SelectLocal.jsx
@@ -1,8 +1,7 @@
-import { useRouter } from 'next/navigation'
+'use client'
 import { useLocal } from '../store/useStore'
 
 export default function SelectLocal() {
-  const router = useRouter()
   const { local, setLocal } = useLocal()
 
   const handleLocalSelection = (selected) => {
@@ -12,25 +11,25 @@ export default function SelectLocal() {
   return (
     <div className="flex pb-3">
       <p
-        className={`m-2 flex cursor-pointer rounded-lg border-none bg-bglight p-2 placeholder-black shadow-light outline-none hover:shadow-hover focus:ring-0 dark:bg-bgdark dark:placeholder-white dark:shadow-dark dark:hover:shadow-hover ${
-          router.pathname === '/viladapenha' ? 'highlighted' : ''
-        } ${local === 'viladapenha' ? 'text-primary' : ''}`}
+        className={`m-2 flex cursor-pointer rounded-lg border-none bg-bglight p-2 placeholder-black shadow-light outline-none hover:shadow-hover focus:ring-0 dark:bg-bgdark dark:placeholder-white dark:shadow-dark dark:hover:shadow-hover  ${
+          local === 'viladapenha' ? 'text-primary' : ''
+        }`}
         onClick={() => handleLocalSelection('viladapenha')}
       >
         Vila da Penha
       </p>{' '}
       <p
-        className={`m-2 flex cursor-pointer rounded-lg border-none bg-bglight p-2 placeholder-black shadow-light outline-none hover:shadow-hover focus:ring-0 dark:bg-bgdark dark:placeholder-white dark:shadow-dark dark:hover:shadow-hover ${
-          router.pathname === '/caxias' ? 'highlighted' : ''
-        } ${local === 'caxias' ? 'text-primary' : ''}`}
+        className={`m-2 flex cursor-pointer rounded-lg border-none bg-bglight p-2 placeholder-black shadow-light outline-none hover:shadow-hover focus:ring-0 dark:bg-bgdark dark:placeholder-white dark:shadow-dark dark:hover:shadow-hover  ${
+          local === 'caxias' ? 'text-primary' : ''
+        }`}
         onClick={() => handleLocalSelection('caxias')}
       >
         Vila Maria Helena
       </p>
       <p
-        className={`m-2 flex cursor-pointer rounded-lg border-none bg-bglight p-2 placeholder-black shadow-light outline-none hover:shadow-hover focus:ring-0 dark:bg-bgdark dark:placeholder-white dark:shadow-dark dark:hover:shadow-hover ${
-          router.pathname === '/tomazinho' ? 'highlighted' : ''
-        } ${local === 'tomazinho' ? 'text-primary' : ''}`}
+        className={`m-2 flex cursor-pointer rounded-lg border-none bg-bglight p-2 placeholder-black shadow-light outline-none hover:shadow-hover focus:ring-0 dark:bg-bgdark dark:placeholder-white dark:shadow-dark dark:hover:shadow-hover  ${
+          local === 'tomazinho' ? 'text-primary' : ''
+        }`}
         onClick={() => handleLocalSelection('tomazinho')}
       >
         Tomazinho
--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import Cookies from 'js-cookie'
 
 export const useSearch = create((set) => ({
   search: '',
@@ -10,10 +11,22 @@ export const useData = create((set) => ({
   setData: (state) => set({ data: state }),
 }))
 
-export const useLocal = create((set) => ({
-  local: 'viladapenha',
-  setLocal: (state) => set({ local: state }),
-}))
+export const useLocal = create((set) => {
+  const cookieValue = Cookies.get('local')
+
+  const decodedValue = decodeURIComponent(cookieValue)
+
+  const initialValue = decodedValue || 'viladapenha'
+
+  return {
+    local: initialValue,
+    setLocal: (state) => {
+      set({ local: state })
+
+      Cookies.set('local', state, { expires: 7 })
+    },
+  }
+})
 
 export const useLoading = create((set) => ({
   loading: true,
