2ee415069ba57b3e1832006c8aeb3b048bec889d
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2024-03-18 01:37:51 -0300

    ...

--- src/components/NewSlider.jsx
+++ src/components/NewSlider.jsx
@@ -72,7 +72,6 @@ export default function NewSlider() {
                     <Link
                       key={item.id}
                       href={`/noticias/${item.page}/${item.id}`}
-                      className="overflow-hidden"
                     >
                       <Image
                         src={item.coverUrl}
@@ -81,10 +80,6 @@ export default function NewSlider() {
                         className=" h-full rounded-[35px] object-fill md:rounded-xl"
                         alt="..."
                       />
-
-                      <h1 className="relative -top-[100px] ml-[20px] inline-flex items-center rounded-xl bg-gradient-to-r  from-slate-950 to-blue-900 px-4 text-xl font-bold text-white md:-top-[150px] md:p-[5px] md:text-3xl">
-                        {item.title}
-                      </h1>
                     </Link>
                   </SwiperSlide>
                 ))}
