1bc00c108a781a3e1d62aa10b6f8d22e9ac9b005
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2024-03-18 22:29:34 -0300

    .

--- src/actions/HandleLocal.js
+++
@@ -1,7 +0,0 @@
-'use server'
-import { useLocal } from '@/store/useStore'
-export const handleLocalSelection = (selected) => {
-  const setLocal = useLocal.getState().setLocal
-
-  setLocal(selected)
-}
