53a324a34893bef68f3326482d2ebb2f9435dcd9
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-03-28 20:16:48 -0300

    ...

--- src/app/reset-password/[token]/page.jsx
+++ src/app/reset-password/[token]/page.jsx
@@ -14,7 +14,7 @@ export default function ResetPasswordPage({ params }) {
       `/reset-password`,
       {
         token,
-        password,
+        newPassword: password,
       },
       {
         headers: {
