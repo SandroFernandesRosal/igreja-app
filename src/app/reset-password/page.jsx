c2533c4ee07bfb97e0c06fcc8e6d8b1ece129afe
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-03-29 17:37:10 -0300

    ...

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -1,49 +1,7 @@
-'use client'
+import ResetPasswordComponent from '@/components/ResetPasswordComponent'
+import { getUserIgreja } from '@/lib/getUserIgreja'
 
-import { useState } from 'react'
-import { api } from '@/lib/api'
-
-export default function ResetPassword({ params }) {
-  const token = params.token
-  const [password, setPassword] = useState('')
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    const response = await api.post(
-      `/reset-password`,
-      {
-        token,
-        password,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    )
-
-    if (response) {
-      console.log('Senha redefinida com sucesso')
-    } else {
-      console.error('Erro ao redefinir a senha')
-    }
-  }
-
-  return (
-    <div className="min-h-scren flex flex-col items-center justify-center pt-60">
-      <h1>Recuperação de Senha</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Nova senha"
-          className="mb-3 rounded-lg border-none ring-0 focus:border-none focus:text-textlight  focus:ring-0  dark:text-black focus:dark:text-black"
-        />
-        <button type="submit">Redefinir Senha</button>
-      </form>
-    </div>
-  )
+export default function ResetPassword() {
+  const userIgreja = getUserIgreja()
+  return <ResetPasswordComponent userIgreja={userIgreja} />
 }
