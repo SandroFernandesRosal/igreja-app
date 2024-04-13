9e0e4b999469db1e614f31a3ae4553ba5c21f50d
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-04-13 18:23:26 -0300

    .

--- src/components/TimeLineItem.jsx
+++ src/components/TimeLineItem.jsx
@@ -9,7 +9,7 @@ export default function TimeLineItem({ title, hora, day, id }) {
   return (
     <>
       <article
-        className={`mb-5  flex h-[300px]  max-w-[150px] flex-col items-center  ${
+        className={`mb-5  flex h-[300px] w-[150px] max-w-[150px] flex-col items-center  ${
           token ? 'justify-between' : 'justify-start gap-5 pt-1'
         } rounded-lg bg-bglight pb-1 shadow-light   transition  delay-150 duration-300  ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-hover dark:bg-bgdark dark:shadow-dark dark:hover:shadow-hover  md:h-[300px] md:w-[200px]  md:max-w-[200px]`}
       >
