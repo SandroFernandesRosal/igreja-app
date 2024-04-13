b47a7e29919858b1ad7bdc4f5d34b64afb4e2644
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-04-13 18:20:17 -0300

    .

--- src/components/TestemunhoLine.jsx
+++ src/components/TestemunhoLine.jsx
@@ -83,7 +83,7 @@ export default function TestemunhoLine({ userIgreja }) {
 
   return (
     <>
-      <section className="mb-4 flex min-h-screen w-[100vw] flex-col items-center rounded-[35px] bg-bglightsecundary  px-1  pb-4 shadow-light dark:bg-bgdarksecundary  dark:shadow-dark md:w-[90vw] md:rounded-xl">
+      <section className="mb-8 flex min-h-screen w-[100vw] flex-col items-center rounded-[35px] bg-bglightsecundary  px-1  pb-4 shadow-light dark:bg-bgdarksecundary  dark:shadow-dark md:w-[90vw] md:rounded-xl">
         <div className="flex flex-col items-center  md:min-w-[35%]">
           <h1 className="m-0 text-lg font-bold text-primary dark:text-secundary ">
             Testemunhos
