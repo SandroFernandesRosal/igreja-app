613728be1391ff940ffbe8f279f61084d4759efe
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-04-22 20:35:58 -0300

    correção de bug

--- src/app/testemunhos/page.jsx
+++ src/app/testemunhos/page.jsx
@@ -1,8 +1,9 @@
-'use client'
+import { getUserIgreja } from '@/lib/getUserIgreja'
 
 import TestemunhoLine from '@/components/TestemunhoLine'
 
-export default function Testemunhos({ userIgreja }) {
+export default function Testemunhos() {
+  const userIgreja = getUserIgreja()
   return (
     <main className="mb-2 mt-4 flex min-h-screen flex-col items-center gap-5 pt-24 md:mt-0 md:pt-[165px]">
       <TestemunhoLine userIgreja={userIgreja} />
