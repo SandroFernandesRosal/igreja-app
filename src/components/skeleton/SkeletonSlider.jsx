10761a61bdbafc381c9f208525dd54b13692dc18
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-04-30 21:26:38 -0300

    mudanças de estilo

--- src/components/skeleton/SkeletonSlider.jsx
+++ src/components/skeleton/SkeletonSlider.jsx
@@ -1,5 +1,5 @@
 export default function SkeletonSlider() {
   return (
-    <div className="z-10 my-5 h-[300px]  w-[100%] max-w-[500px]  animate-pulse  rounded-[35px] bg-gray-700  shadow-light dark:shadow-dark md:mb-10  md:mt-5 md:h-[400px] md:w-[500px]  md:rounded-xl md:shadow-lg lg:h-[500px]    lg:w-[95%] "></div>
+    <div className="z-10 my-2 h-[300px] w-[100%] max-w-[500px]  animate-pulse rounded-[35px]  bg-gray-700  shadow-light dark:shadow-dark  md:mb-10 md:mt-5 md:h-[400px]  md:w-[500px] md:rounded-xl md:shadow-lg  lg:h-[500px] lg:w-[95%] lg:p-2    lg:py-5 "></div>
   )
 }
